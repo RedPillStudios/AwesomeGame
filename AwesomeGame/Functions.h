599fdaeb18fc7c6d0f0aa730eb24f5287b759122
Roger Leon Borras <36153923+rleonborras@users.noreply.github.com>
2018-03-13 00:28:58 +0100

    Added_PArallax_effect&&Start Menu&&Explosions.

--- AwesomeGame/Functions.h
+++ AwesomeGame/Functions.h
@@ -26,12 +26,16 @@ bool init() {
 	return succes;
 }
 
-void Draw(int SpriteColumn1, int SpriteColumn2, int SpriteColumn3, int PosSprite, int Menuout,int frames) {
+void Draw(int SpriteColumn1, int SpriteColumn2, int SpriteColumn3, int PosSprite, int Menuout, int frames, int frameExplosion, bool Mainmenu) {
 
 	SDL_RenderClear(Main_Renderer);
-
+	
+	SDL_RenderCopy(Main_Renderer, LastBackground_Texture, NULL, &LastBackground);
+	SDL_RenderCopy(Main_Renderer, LastBackground_Texture, NULL, &LastBackground2);
 	SDL_RenderCopy(Main_Renderer, Background_Texture, NULL, &Background);
 	SDL_RenderCopy(Main_Renderer, Background_Texture, NULL, &Background2);
+	SDL_RenderCopy(Main_Renderer, Foreground_Texture, NULL, &Foreground);
+	SDL_RenderCopy(Main_Renderer, Foreground_Texture, NULL, &Foreground2);
 	SDL_RenderCopy(Main_Renderer, Impulsor_Texture, &Impulsor_Clipping[frames/4], &Impulsor);
 	SDL_RenderCopy(Main_Renderer, Player_shipTexture, NULL, &Ship);
 	SDL_RenderCopy(Main_Renderer, Shoot_Texture, NULL, &Shoot);
@@ -42,18 +46,26 @@ void Draw(int SpriteColumn1, int SpriteColumn2, int SpriteColumn3, int PosSprite
 	SDL_RenderCopy(Main_Renderer, Enemy2_ShotTexture, NULL, &Enemy3Shoot);
 	SDL_RenderCopy(Main_Renderer, Enemy2_shipTexture, NULL, &Enemy4);
 	SDL_RenderCopy(Main_Renderer, Enemy2_ShotTexture, NULL, &Enemy4Shoot);
-	SDL_RenderCopy(Main_Renderer, Score_Texture, NULL, &Score);
-	SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn3], &Num_1);
-	SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn2], &Num_2);
-	SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn1], &Num_3);
-	SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[PosSprite], &Num_4);
+	SDL_RenderCopy(Main_Renderer, Explosion_Texture, &Explosion_Clipping[frameExplosion / 4], &Explosion);
+	if (!Mainmenu) {
+		SDL_RenderCopy(Main_Renderer, Score_Texture, NULL, &Score);
+		SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn3], &Num_1);
+		SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn2], &Num_2);
+		SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[SpriteColumn1], &Num_3);
+		SDL_RenderCopy(Main_Renderer, Numbers_Texture, &NumClipper[PosSprite], &Num_4);
+	}
 	if (Menuout) {
+		SDL_RenderCopy(Main_Renderer, Background_Dead, NULL, NULL);
 		SDL_RenderCopy(Main_Renderer, GameOver_Texture, NULL, &GameOver);
 		SDL_RenderCopy(Main_Renderer, TryAgain_Texture, NULL, &TryAgain);
 		SDL_RenderCopy(Main_Renderer, Yes_texture, NULL, &Yes);
 		SDL_RenderCopy(Main_Renderer, No_Texture, NULL, &No);
 		SDL_RenderCopy(Main_Renderer, Selector_texture, NULL, &Selector);
 	}
+	if(Mainmenu){
+		SDL_RenderCopy(Main_Renderer, AwesomeGame_Texture, NULL, &AwesomeGame);
+		SDL_RenderCopy(Main_Renderer, PressEnter_Texture, NULL, &Press);
+		SDL_RenderCopy(Main_Renderer, Start_Texture, NULL, &Start); }
 	SDL_RenderPresent(Main_Renderer);
 }
 
@@ -81,7 +93,29 @@ void SpriteSheet(SDL_Rect Sprite[]) {
 	}
 
 }
+void SpriteSheetExplosion(SDL_Rect Sprite[]) {
 
+	for (int i = 1; i < EXPLOSION_FRAMES; ++i) {
+		if (i < 5) {
+			Sprite[i].x = Sprite[i - 1].x + 113;
+			Sprite[i].y = 0;
+			Sprite[i].w = 113;
+			Sprite[i].h = 114;
+		}
+		else if (i==5){
+			Sprite[i].x = 0;
+			Sprite[i].y = 114;
+			Sprite[i].w = 113;
+			Sprite[i].h = 114;
+		}
+		else if(i>5){
+			Sprite[i].x = Sprite[i - 1].x + 113;
+			Sprite[i].y = 114;
+			Sprite[i].w = 113;
+			Sprite[i].h = 114;
+		}
+	}
+}
 
 
 #endif // !1
--- AwesomeGame/Textures.h
+++ AwesomeGame/Textures.h
@@ -3,7 +3,9 @@
 
 SDL_Window* window = nullptr;
 SDL_Renderer* Main_Renderer = nullptr;
+SDL_Texture* Foreground_Texture = nullptr;
 SDL_Texture* Background_Texture = nullptr;
+SDL_Texture* LastBackground_Texture = nullptr;
 SDL_Texture* Player_shipTexture = nullptr;
 SDL_Texture* Enemy_shipTexture = nullptr;
 SDL_Texture* Enemy2_shipTexture = nullptr;
@@ -11,12 +13,17 @@ SDL_Texture* Enemy2_ShotTexture = nullptr;
 SDL_Texture* Shoot_Texture = nullptr;
 SDL_Texture* Score_Texture = nullptr;
 SDL_Texture* Numbers_Texture = nullptr;
+SDL_Texture* PressEnter_Texture = nullptr;
+SDL_Texture* Start_Texture = nullptr;
+SDL_Texture* AwesomeGame_Texture = nullptr;
 SDL_Texture* GameOver_Texture = nullptr;
 SDL_Texture* TryAgain_Texture = nullptr;
 SDL_Texture* Yes_texture = nullptr;
 SDL_Texture* No_Texture = nullptr;
 SDL_Texture* Selector_texture = nullptr;
 SDL_Texture* Impulsor_Texture = nullptr;
+SDL_Texture* Background_Dead = nullptr;
+SDL_Texture* Explosion_Texture = nullptr;
 
 SDL_Texture* loadTexture(const char* filename) {
 
@@ -35,8 +42,13 @@ SDL_Texture* loadTexture(const char* filename) {
 bool loadMedia() {
 
 	bool succes = true;
+	Start_Texture = loadTexture("Text/Start.png");
+	PressEnter_Texture = loadTexture("Text/Main_Title_Press.png");
+	AwesomeGame_Texture = loadTexture("Text/Titles.png");
 	Shoot_Texture = loadTexture("Images/Plasma_Shoot.png");
-	Background_Texture = loadTexture("Images/Background.png");
+	Foreground_Texture = loadTexture("Images/Foreground.png");
+	Background_Texture = loadTexture("Images/Parallax_Background.png");
+	LastBackground_Texture = loadTexture("Images/End_Background.png");
 	Player_shipTexture = loadTexture("Images/Player_Ship.png");
 	Enemy_shipTexture = loadTexture("Images/Enemy_Ship.png");
 	Score_Texture = loadTexture("Text/Score.png");
@@ -49,8 +61,8 @@ bool loadMedia() {
 	No_Texture = loadTexture("Text/NO.png");
 	Selector_texture = loadTexture("Text/Selector.png");
 	Impulsor_Texture = loadTexture("Images/Impuls.png");
-
-
+	Background_Dead = loadTexture("Images/Background_GameOver.png");
+	Explosion_Texture = loadTexture("Images/Explosion.png");
 	if (Background_Texture == NULL || Player_shipTexture == NULL || Enemy_shipTexture == NULL || Shoot_Texture == NULL || Score_Texture == NULL || Numbers_Texture == NULL) {
 		succes = false;
 	}
